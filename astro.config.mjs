29f58defe04fee324bdc8f2a02e82378e00bdcb9
Krystian Nowakowski <krysti.8642@gmail.com>
2024-11-07 10:42:22 +0100

    github pages astro deployment actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://kabaczek1.github.io",
+  base: "kaba-blog",
+});
